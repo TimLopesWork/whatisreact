f377de33e81ab512ae801621774827e6644f5d6b
Tim Lopes <75840919+TimLopesWork@users.noreply.github.com>
2022-08-08 20:00:01 +0300

    Created new main.js
    Fixed dynamic paths

---
+++ main.js
@@ -0,0 +1,14 @@
+ // Declearing homepath
+ const home = "https://timlopeswork.github.io/whatisreact/"; 
+ // Getting the dynamic path from url
+ const thepath = window.location.pathname;
+ console.log(thpath);
+ const root = document.getElementById('root');
+ // Simulation a react component HomePage
+ const HomePage =()=>`<h1>hello World!</h1><p> I am HomePage</p><a href='https://timlopeswork.github.io/whatisreact/about.html'>>About</a>`;
+ // Simulation a react component AboutPage
+ const AboutPage =()=>`<h1>About</h1><a href='https://timlopeswork.github.io/whatisreact/index.html'>Home</a>`;
+ // Simulation a react main exit point
+ const Index =()=>{
+      const isHome = thepath === home ? root.innerHTML=HomePage() : root.innerHTML=AboutPage();
+ }; Index();
