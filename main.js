8846cb9018245c14107116adf6c3e4f2147ef9f0
Tim Lopes <75840919+TimLopesWork@users.noreply.github.com>
2022-08-08 20:17:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,12 @@
  // Simulation a react component HomePage
  const HomePage =()=>`<h1>hello World!</h1><p> I am HomePage</p><a href='https://timlopeswork.github.io/whatisreact/about.html'>>About</a>`;
  // Simulation a react component AboutPage
- const AboutPage =()=>`<h1>About</h1><a href='https://timlopeswork.github.io/whatisreact/index.html'>Home</a>`;
+ const AboutPage =()=>`<h1>About</h1><a href='https://timlopeswork.github.io/whatisreact/'>Home</a>`;
  // Simulation a react main exit point
  const Index =()=>{
-      const isHome = thepath === home || thepath === home+"index.html" ? root.innerHTML=HomePage() : root.innerHTML=AboutPage();
+      if(thepath !== home || thepath !== home+"index.html"){
+       root.innerHTML=AboutPage();
+      }else{
+       ? root.innerHTML=HomePage();
+      }
  }; Index();
